2aa27af4e7135764d7453dc39e2452d6a69cf460
Michael Metcalf <m.metcalf6453@gmail.com>
2021-11-16 14:04:43 -0800

    fixed map

--- javascript/loc_app.js
+++ javascript/loc_app.js
@@ -7,23 +7,31 @@ const isItOpenChicago = document.getElementById('openChicago');
 const isItOpenBoston = document.getElementById('openBoston');
 
 const date = new Date();
-const day = date.getDay();
-const myHour = date.getHours();
-// const hour = day.getTimezoneOffset();
-// const hour = myHour.getTimezoneOffset();
+const day = date.getUTCDay();
+const hours = date.getHours();
+const hour = date.getUTCHours();
+// const trueDate = date - (date.getTimezoneOffset()/60);
+// const nowDate = trueDate.toDateString();
+
+// const day = trueDate.getDay();
+// const hour = trueDate.getHours();
+console.log(date, day, hour);
+console.log(hours, hour);
 
 
-console.log(date, day, myHour);
+
+
 
 function openYetSeattle() {
     if (day == '0' || day == '6') {
-        if (hour > 9 && hour < 14) {
+        if (hour >= 16 && hour < 23) {
             isItOpenSeattle.textContent = 'OPEN';
         } else {
             isItOpenSeattle.textContent = 'CLOSED';
         }
-    } else {
-        if (hour > 7 && hour < 14) {
+    }
+     else {
+        if (hour >= 15 && hour < 22) {
             isItOpenSeattle.textContent = 'OPEN';
         } else {
             isItOpenSeattle.textContent = 'CLOSED';
@@ -33,13 +41,13 @@ function openYetSeattle() {
 //GMT-6
 function openYetAustin() {
     if (day == '0' || day == '6') {
-        if (hour > 9 && hour < 14) {
+        if (hour >= 15 && hour < 20) {
             isItOpenAustin.textContent = 'OPEN';
         } else {
             isItOpenAustin.textContent = 'CLOSED';
         }
     } else {
-        if (hour > 7 && hour < 14) {
+        if (hour >= 13 && hour < 20) {
             isItOpenAustin.textContent = 'OPEN';
         } else {
             isItOpenAustin.textContent = 'CLOSED';
@@ -49,13 +57,13 @@ function openYetAustin() {
 
 function openYetTampa() {
     if (day == '0' || day == '6') {
-        if (hour > 9 && hour < 14) {
+        if (hour >= 13 && hour < 22) {
             isItOpenTampa.textContent = 'OPEN';
         } else {
             isItOpenTampa.textContent = 'CLOSED';
         }
     } else {
-        if (hour > 7 && hour < 14) {
+        if (hour >= 11 && hour < 21) {
             isItOpenTampa.textContent = 'OPEN';
         } else {
             isItOpenTampa.textContent = 'CLOSED';
@@ -65,13 +73,13 @@ function openYetTampa() {
 
 function openYetChicago() {
     if (day == '0' || day == '6') {
-        if (hour > 9 && hour < 14) {
+        if (hour >= 14 && hour < 22) {
             isItOpenChicago.textContent = 'OPEN';
         } else {
             isItOpenChicago.textContent = 'CLOSED';
         }
     } else {
-        if (hour > 7 && hour < 14) {
+        if (hour >= 13 && hour < 20) {
             isItOpenChicago.textContent = 'OPEN';
         } else {
             isItOpenChicago.textContent = 'CLOSED';
@@ -81,13 +89,13 @@ function openYetChicago() {
 
 function openYetBoston() {
     if (day == '0' || day == '6') {
-        if (hour > 9 && hour < 14) {
+        if (hour >= 14 && hour < 19) {
             isItOpenBoston.textContent = 'OPEN';
         } else {
             isItOpenBoston.textContent = 'CLOSED';
         }
     } else {
-        if (hour > 7 && hour < 14) {
+        if (hour >= 12 && hour < 19) {
             isItOpenBoston.textContent = 'OPEN';
         } else {
             isItOpenBoston.textContent = 'CLOSED';
@@ -95,14 +103,8 @@ function openYetBoston() {
     }
 }
 
-
-
-
 openYetSeattle();
 openYetAustin();
 openYetTampa();
 openYetChicago();
 openYetBoston();
-
-
-
